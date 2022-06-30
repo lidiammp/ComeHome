734b6cdb213d9ea920902efd94cc4d66d6170892
lidiammp <102434840+lidiammp@users.noreply.github.com>
2022-06-30 12:56:12 -0700

    Grab and Throw

--- Come Home/Assets/scripts/Movement.cs
+++ Come Home/Assets/scripts/Movement.cs
@@ -5,26 +5,22 @@ public class Movement : MonoBehaviour
 {
     public float MovementSpeed = 5;
     public float JumpForce = 5;
-        private Rigidbody2D rb;
+    private Rigidbody2D rb;
 
-  private void Start()
+   private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-       
     }
 
-
-     void Update()
+    
+   private void Update()
     {
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
-        
+
         if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.001f)
         {
             rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
         }
     }
-       
-
-    
 }
